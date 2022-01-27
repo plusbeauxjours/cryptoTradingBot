e2f7a49c5ba4aebc353852fa1ec5592f107b3ae7
plusbeauxjours <plusbeauxjours@gmail.com>
2022-01-27 23:47:05 +0700

    set: backtest

--- backtest.py
+++ backtest.py
@@ -1,7 +1,7 @@
 import pyupbit
 import numpy as np
 
-df = pybithumb.get_ohlcv("KRW-BTC", count=7)
+df = pyupbit.get_ohlcv("KRW-BTC", count=7)
 df['range'] = (df['high'] - df['low']) * 0.5
 df['target'] = df['open'] + df['range'].shift(1)
 
@@ -14,3 +14,5 @@ df['hpr'] = df['ror'].cumprod()
 df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
 print("MDD(%): ", df['dd'].max())
 df.to_excel("dd.xlsx")
+
+
