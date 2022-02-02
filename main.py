560e562567beea7db243231a6d034ba614cba604
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-02 22:49:33 +0700

    set: test CICD

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ AccessKey=os.environ.get('AccessKey')
 SecretKey=os.environ.get('SecretKey')
 upbit = pyupbit.Upbit(AccessKey, SecretKey)
 
+print('TEST')
 print(upbit.get_balance("KRW-BTC"))
 print(upbit.get_balance("KRW"))
